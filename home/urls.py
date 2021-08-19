d722697c6b6d33aae54594a56f52c735dccf63ce
lucamartinatti <77723140+lucamartinatti@users.noreply.github.com>
2021-08-19 08:41:02 +0200

    Initialize django project

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    #path('', views.index, name='home-page'),
+    path('search_results', views.search_results, name='search-results'),
+]
